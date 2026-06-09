a72a1e109445362a027ae0006d30331c156d2cfb
Vikas Yadav <vikasyadav91400@gmail.com>
2026-06-09 16:33:01 +0530

    Add files via upload

---
+++ BasicConcepts/if_else_ststments.py
@@ -0,0 +1,23 @@
+#a=int(input("Enter your age: "))
+# Conditional statements
+# >, <, >=(greater than or equal to), <=(less than or equal to), ==(equal to), !=(not equal to)
+#if a>=18:
+    #print("You are eligible to vote")
+#else:
+    #print("You are not eligible to vote")
+    
+appleprice=190
+budget=200
+if appleprice<=budget:
+    print("You can buy the apple")  
+else:
+    print("You cannot buy the apple")  
+    
+#elif statement
+num = int(input("Enter the value of num: "))    
+if num<0:
+    print("The number is negative")
+elif num==0:
+    print("The number is zero")
+else:
+    print("The number is positive")
