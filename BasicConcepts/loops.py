8732e2b2e15868c827372e6193460408c709b052
Vikas Yadav <vikasyadav91400@gmail.com>
2026-06-16 10:42:25 +0530

    Add files via upload

---
+++ BasicConcepts/loops.py
@@ -0,0 +1,39 @@
+#for loop
+#iterating over a string:
+#name = "vikas"
+#for i in name:
+    #print(i)
+    #if(i == "k"):
+        #print("This is something special")
+#iterating over a list:
+#colours = ["red", "green", "blue", "yellow"]
+#for i in colours:
+     #print(i)
+     #for k in i:
+        # print(k)
+       
+#range function()
+#for i in range(1,10):
+    #print(i)       
+    
+#while loop
+# i = int(input("Enter a number: "))
+# while(i<=20): 
+#     i = int(input("Enter a number: "))
+#     print(i)
+#     print("Done with the loop")
+    
+count = 5
+while(count>1):
+    print(count)
+    count = count - 1
+else:
+    print("Done with the loop")
+    
+    
+    
+    
+       
+    
+
+    
