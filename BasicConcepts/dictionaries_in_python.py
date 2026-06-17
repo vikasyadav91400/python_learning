208f97f954e1094f168fc6ea7718e718b68caebe
Vikas Yadav <vikasyadav91400@gmail.com>
2026-06-17 13:58:44 +0530

    Add files via upload

---
+++ BasicConcepts/dictionaries_in_python.py
@@ -0,0 +1,20 @@
+info = {'Name':'Vikas','age':20,'class':'btech'}
+      
+# print(info) 
+# print(type(info))
+# #update
+# info.update({'age':22})
+# info.update({'DOB':2006})
+# print(info)
+# #clear
+# info.clear()
+# print(info)
+# # pop
+# # info.pop('age')
+# # print(info)
+# #popitem
+# info.popitem
+# print(info)
+#del
+del info['age']
+print(info)
