76bca0d3f691b23125d904415fc7a9b06ab6e33f
Vikas Yadav <vikasyadav91400@gmail.com>
2026-06-23 18:10:14 +0530

    Add files via upload

---
+++ BasicConcepts/break_continue_statments.py
@@ -0,0 +1,10 @@
+# for i in range(12):
+#     if(i==10):
+#             break
+#     print("5 *",i+1,"=",5*(i+1))
+
+for i in range(12):
+    if(i==10):
+            continue
+    print("5 *",i,"=",5*(i))
+    
