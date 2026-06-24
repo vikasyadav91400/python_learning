c7f962e27cf24a543d6eaa19fab7693ed58b8ab9
Vikas Yadav <vikasyadav91400@gmail.com>
2026-06-24 11:44:35 +0530

    Add files via upload

---
+++ BasicConcepts/enumerate_function.py
@@ -0,0 +1,22 @@
+marks = [11, 34, 57, 88, 76, 55 ,90 ,87]
+
+# index = 0
+# for mark in marks:
+#     print(mark)
+#     if(index == 3):
+#         print("Very good")
+#     index += 1
+
+# for index, mark in enumerate(marks):
+#      print(mark)
+#      if(index == 3):
+#          print("Very good")
+#      if(index == 5):
+#         print("Good")
+     
+fruits=['Apple', 'Banana', 'Mango', 'Orange']
+for index, fruit in enumerate(fruits):
+    print(index,fruit)     
+
+
+
