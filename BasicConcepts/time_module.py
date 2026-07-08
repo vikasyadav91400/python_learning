ed3379817ce164fd8880c8455735eecfac25e088
Vikas Yadav <vikasyadav91400@gmail.com>
2026-07-08 10:01:36 +0530

    Add files via upload

---
+++ BasicConcepts/time_module.py
@@ -0,0 +1,32 @@
+# import time
+# print(time.time())
+#time.time
+import time
+# def usingWhile():
+#     i=0
+#     while(i<50000):
+        
+#         i = i + 1
+#         print(i)
+        
+# def usingFor():
+#     for i in range(50000):
+               
+#         print(i)        
+        
+# init = time.time()
+# usingFor()
+# t1=time.time() - init
+# init = time.time()
+# usingWhile()
+# print(t1)
+# print(time.time() - init)
+
+# #time.sleep()
+
+
+
+print("Hello")
+time.sleep(3)
+print("This is printed after 3 seconds")
+           
