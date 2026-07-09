5d835af64f87909b9219052dfcc2f9c2900b5948
Vikas Yadav <vikasyadav91400@gmail.com>
2026-07-09 09:40:18 +0530

    Add files via upload

---
+++ BasicConcepts/warlus_operator.py
@@ -0,0 +1,16 @@
+#without using walrus operator
+
+# foods= list()
+# while True:
+#     food= input("Enter the food you like to eat: ")
+#     if food== "quit":
+#         break
+#     foods.append(foods)
+    
+#using walrus operator 
+
+foods= list()
+while(food:= input("Enter the food you like to eat: ")) != "quit":
+        foods.append(food)
+    
+    
